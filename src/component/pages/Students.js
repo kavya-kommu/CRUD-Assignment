ec70d93819d2619da0a8fa9502ae8de714ac4331
kavyakommu <kavyakommu07@gmail.com>
2022-11-29 18:29:50 +0530

    first commit

--- src/component/pages/Students.js
+++ src/component/pages/Students.js
@@ -1,8 +1,8 @@
-import React, {useState,useEffect} from "react";
-import axios from "axios";
+import React, {useEffect} from "react";
+
 import {Link} from "react-router-dom";
 const Students =() =>{
-    const [students,setStudents]=useState([]);
+    
     useEffect(()=>{
         console.log("My Channel");
 
